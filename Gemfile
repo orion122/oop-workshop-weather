a06b52edda9fc545e8c835830ecba3798b9c467c
ramazan yaraliev <yaru@bk.ru>
2019-02-10 09:52:54 +0300

    first version

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
 
+gem 'slop'
+
 # Specify your gem's dependencies in weather.gemspec
 gemspec
--- lib/weather.rb
+++ lib/weather.rb
@@ -1,6 +1,33 @@
-require "weather/version"
+require 'weather/version'
+require 'net/http'
+require 'json'
+
 
 module Weather
-  class Error < StandardError; end
-  # Your code goes here...
+  class Weather
+    SERVICES = {
+      'first'=> 'https://www.metaweather.com/api/location/search/?query=',
+      'second'=> 'http://api.apixu.com/v1/forecast.json?key=be33701f8d2a4beeae864603191002&q='
+    }
+
+    attr_reader :service_url
+
+    def initialize(service: 'first')
+      @service_url = SERVICES[service]
+    end
+
+    def get_info(city: 'moscow')
+      uri = URI(@service_url + city)
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
+    end
+
+    # def first_service_query(city)
+    #   SERVICES['first'] + city
+    # end
+    #
+    # def second_service_query(city)
+    #   SERVICES['second'] + city
+    # end
+  end
 end
