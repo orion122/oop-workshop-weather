77b626087f83d0418346f4d866d6f375c565e5ca
ramazan yaraliev <yaru@bk.ru>
2019-02-10 11:09:37 +0300

    add test

--- spec/weather_spec.rb
+++ spec/weather_spec.rb
@@ -1,9 +1,25 @@
 RSpec.describe Weather do
-  it "has a version number" do
-    expect(Weather::VERSION).not_to be nil
+  let(:city) { 'Moscow' }
+
+  class Service
+    def self.make_request(city)
+      {
+        'city': city,
+        'forecast': '+25'
+      }
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let(:weather) {
+    Weather::Weather.new(
+      'my_service',
+      { 'my_service'=> Service })
+  }
+
+  describe '#get_info' do
+    context 'pass new service' do
+      subject { weather.get_info(city) }
+      it { is_expected.to eq(Service.make_request(city)) }
+    end
   end
 end
