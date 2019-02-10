2d6679dc9015ee5ab7d55d4d5a349546bd97d092
ramazan yaraliev <yaru@bk.ru>
2019-02-10 10:16:00 +0300

    second version

--- lib/weather.rb
+++ lib/weather.rb
@@ -4,30 +4,40 @@ require 'json'
 
 
 module Weather
-  class Weather
-    SERVICES = {
-      'first'=> 'https://www.metaweather.com/api/location/search/?query=',
-      'second'=> 'http://api.apixu.com/v1/forecast.json?key=be33701f8d2a4beeae864603191002&q='
-    }
+  class Metaweather
+    URL = 'https://www.metaweather.com/api/location/search/?query='
 
-    attr_reader :service_url
-
-    def initialize(service: 'first')
-      @service_url = SERVICES[service]
-    end
-
-    def get_info(city: 'moscow')
-      uri = URI(@service_url + city)
+    def make_request(city: 'moscow')
+      uri = URI(URL + city)
       response = Net::HTTP.get(uri)
       JSON.parse(response)
     end
+  end
 
-    # def first_service_query(city)
-    #   SERVICES['first'] + city
-    # end
-    #
-    # def second_service_query(city)
-    #   SERVICES['second'] + city
-    # end
+
+  class Apixu
+    URL = 'http://api.apixu.com/v1/forecast.json?key=be33701f8d2a4beeae864603191002&q='
+
+    def make_request(city: 'moscow')
+      uri = URI(URL + city)
+      response = Net::HTTP.get(uri)
+      JSON.parse(response)
+    end
+  end
+
+
+  class Weather
+    SERVICES = {
+      'metaweather' => ::Weather::Metaweather.new,
+      'apixu' => ::Weather::Apixu.new
+    }
+
+    def initialize(service_name: 'metaweather')
+      @service = SERVICES[service_name]
+    end
+
+    def get_info(city: 'moscow')
+      @service.make_request(city: city)
+    end
   end
 end
